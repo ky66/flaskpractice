96fcee43b66aa0c9bdb3bb0ebcac0ed7adfa0130
ky66 <ky66@duke.edu>
2020-06-15 06:24:40 -0400

    First commit

--- flask_qa/wsgi.py
+++
@@ -1,3 +0,0 @@
-from flask_qa.__init__.py import create_app
-
-app = create_app()
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+from flask_qa.__init__.py import create_app
+
+<<<<<<< HEAD
+app = create_app()
+=======
+app = create_app()
+>>>>>>> First commit
